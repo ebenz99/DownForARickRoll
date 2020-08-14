f51bf8163836a7484b430345da80e1138fbb7a3b
Ethan Bensman <ebenz99@gmail.com>
2020-08-14 13:04:00 -0400

    file reorg

--- index.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-  $('#play-video').on('click', function(ev) {
-    $("#vid")[0].style.display = "block";
-    $("#vid")[0].src += "?autoplay=1&mute=1";
-    ev.preventDefault();
-  });
-});
